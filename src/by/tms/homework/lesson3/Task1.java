ba5a9a02535c855f6578ed277d7e4e5727da3643
Yurymialeshka <energie85@mail.ru>
2022-06-20 13:19:53 +0400

    Homework Lesson3 Task1 ver2.0

--- src/by/tms/homework/lesson3/Task1.java
+++ src/by/tms/homework/lesson3/Task1.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
-        int num = sc.nextInt();
-        int a = 0;
-        for (int i = 1; i <= num; i++) {
-            a += i;
+        int numConsole = scanner.nextInt();
+        int sum = 0;
+        while (numConsole > 0) {
+            sum = sum + numConsole % 10;
+            numConsole = numConsole / 10;
         }
-        System.out.println("Сумма цифр: " + a);
-
+        System.out.println("Сумма чисел : " + sum);
     }
 }
+
+
+
+
+
